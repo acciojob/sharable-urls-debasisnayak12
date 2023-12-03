1389a48668232a2c3d8de6bf2c233f65c872b6a0
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-03 20:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // your code here
-let urlId = document.getElementById("url");
-let formInput = document.getElementById("form");
+const urlId = document.getElementById("url");
+const formInput = document.getElementById("form");
 
 formInput.addEventListener('submit',(event)=>{
 	event.preventDefault();
-	let name = document.getElementById("name").value;
-	let year = document.getElementById("year").value;
+	const name = document.getElementById("name").value;
+	const year = document.getElementById("year").value;
 
 	let queryString = `&name=${name}`;
 	if(year){
 		queryString += `&year=${year}`;
 	}
-	let updateUrl = `https://localhost:8080/${queryString}`;
+	const updateUrl = `https://localhost:8080/${queryString}`;
 	urlId.textContent = updateUrl;
 });
